4cf43095083d070575b853cff42973087dad42ac
Juan122113 <ammiel89@hotmail.com>
2025-02-09 13:49:53 -0300

    Update index.html & main.js
    Adding changes to add link to the project

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function openLightbox(imageSrc) {
+function openLightbox(element) {
     const lightbox = document.querySelector('.lightbox');
-    const lightboxImg = lightbox.querySelector('img');
-    lightboxImg.src = imageSrc;
+    // const lightboxImg = lightbox.querySelector('img');
+    const iframe = lightbox.querySelector("iframe");
+    const visitBtn = lightbox.querySelector(".visit-btn");
+
+    // Obtener datos del proyecto
+    const iframeUrl = element.data.iframeUrl;
+    const projectUrl = element.dataset.projectUrl;
+
+    // Configurar iframe y enlace
+    iframe.src = iframeUrl;
+    visitBtn.href = projectUrl;
+
+    // lightboxImg.src = imageSrc;
     lightbox.classList.add('active');
 }
 
-function closeLightbox() {
-    document.querySelector('.lightbox').classList.remove('active');
+function closeLightbox(event) {
+    if(event.target === document.querySelector(".lightbox")) {
+        const lightbox = document.querySelector(".lightbox");
+        lightbox.querySelector("iframe").src = "https://juan122113.github.io/tip-calculator-app-main/";
+        lightbox.classList.remove("active");
+    }
+    // document.querySelector('.lightbox').classList.remove('active');
 }
+
+// Event listeners para los proyectos
+document.querySelectorAll(".project").forEach(project => {
+    project.addEventListener("click", () => openLightbox(project));
+});
